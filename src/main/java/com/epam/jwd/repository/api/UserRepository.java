f496a5d9fb641027594425b65569fc080b9394fe
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-09-17 23:46:25 +0300

    Fill UserRepository

--- src/main/java/com/epam/jwd/repository/api/UserRepository.java
+++ src/main/java/com/epam/jwd/repository/api/UserRepository.java
@@ -2,5 +2,13 @@ package com.epam.jwd.repository.api;
 
 import com.epam.jwd.repository.entity.Person;
 
+import java.util.List;
+
 public interface UserRepository<V, T extends Person<V>> {
+
+    T findById(V id);
+    T findByName(String name);
+    boolean removeUser(String name);
+    void save(T user);
+    List<T> findByGender(String gender);
 }
