46019d47591cde10556f0909bc5e41273590c4d2
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-09-25 16:36:00 +0300

    Add operatorName field to class

--- src/main/java/com/epam/jwd/service/logic/OperatorLoader.java
+++ src/main/java/com/epam/jwd/service/logic/OperatorLoader.java
@@ -8,27 +8,29 @@ import org.apache.logging.log4j.Logger;
 public class OperatorLoader implements Runnable{
 
     private final CallCenterService callCenterService;
+    private final String operatorName;
 
     private static final Logger log = LogManager.getLogger(OperatorLoader.class);
 
-    private static final String SERVING_USER_INFO_MESSAGE = "User %s is serving,\n his/her objective is %s,\n his/her age is %d,\n his/her gender is %s\n";
+    private static final String SERVING_USER_INFO_MESSAGE = "User %s is serving by Operator=%s,\n his/her objective is %s,\n his/her age is %d,\n his/her gender is %s\n";
     private static final String SERVING_USER_LOG_MESSAGE = "User has been gotten from queue by operator";
     private static final String INTERRUPTED_EXCEPTION_LOG_MESSAGE = "Thread has been interrupted";
 
 
-    public OperatorLoader(CallCenterService callCenterService) {
+    public OperatorLoader(CallCenterService callCenterService, String operatorName) {
         this.callCenterService = callCenterService;
+        this.operatorName = operatorName;
     }
 
     @Override
     public void run() {
         while(true) {
             try {
+                Thread.sleep(10000);
                 User user = callCenterService.getUserFromTheQueue();
                 log.debug(SERVING_USER_LOG_MESSAGE);
-                System.out.printf(SERVING_USER_INFO_MESSAGE, user.getPersonName(), user.getVisitAim(),
+                System.out.printf(SERVING_USER_INFO_MESSAGE, operatorName, user.getPersonName(), user.getVisitAim(),
                         user.getAge(), user.getGender().toString());
-                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 log.error(INTERRUPTED_EXCEPTION_LOG_MESSAGE, e);
