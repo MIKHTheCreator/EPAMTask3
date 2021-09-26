df2687e2518e701d0b157480636b701dccefd59f
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-09-26 18:20:27 +0300

    Create normal view

--- src/main/java/com/epam/jwd/controller/Controller.java
+++ src/main/java/com/epam/jwd/controller/Controller.java
@@ -1,11 +1,18 @@
 package com.epam.jwd.controller;
 
 import com.epam.jwd.service.director.ApplicationStarter;
+import com.epam.jwd.view.Menu;
 
 public class Controller {
 
+    private static final int NUM_OF_OPERATORS = 4;
+
     public static void main(String[] args) {
         ApplicationStarter starter = new ApplicationStarter();
-        starter.start(9);
+        Menu menu = new Menu();
+
+        menu.getStarterMenu();
+        menu.getUserInput();
+        starter.start(NUM_OF_OPERATORS);
     }
 }
---
+++ src/main/java/com/epam/jwd/view/Menu.java
@@ -0,0 +1,34 @@
+package com.epam.jwd.view;
+
+import java.util.Scanner;
+
+public class Menu {
+
+    private static final String STARTER_MENU = """
+            ////////////////MULTI-THREADING CALLCENTER IS GLAD TO SEE YOU\\\\\\\\\\\\\\\\\
+            This is a test multi-threading application with little amount of opportunities
+            You have only one:
+            1.Choose the number of operators of this CallCenter
+            Now we have 4 active operators in our center!!!
+            
+            """;
+    private static final String LETS_START = "//Enter any key to start:\n";
+    private static final String APPLICATION_STARTING = "Application starting, enjoy the result!\n\n\n";
+
+
+    public void getStarterMenu() {
+        System.out.println(STARTER_MENU);
+    }
+
+    public void getUserInput() {
+        System.out.println(LETS_START);
+
+        Scanner scanner = new Scanner(System.in);
+
+        if(scanner.hasNextLine()) {
+            System.out.println(APPLICATION_STARTING);
+        }
+
+    }
+
+}
