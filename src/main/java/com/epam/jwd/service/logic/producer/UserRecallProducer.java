2ec87dff4bde7af2ef3c9d50bd4a770a9d410f68
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-09-26 14:48:05 +0300

    Add recall producer

--- src/main/java/com/epam/jwd/service/logic/producer/UserRecallProducer.java
+++ src/main/java/com/epam/jwd/service/logic/producer/UserRecallProducer.java
@@ -1,5 +1,34 @@
 package com.epam.jwd.service.logic.producer;
 
-public class UserRecallProducer {
+import com.epam.jwd.repository.entity.User;
+import com.epam.jwd.service.api.CallCenterService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+
+public class UserRecallProducer implements Runnable{
+
+    private final CallCenterService callCenterService;
+
+    private static final Logger log = LogManager.getLogger(UserRecallProducer.class);
+    private static final String INTERRUPTED_EXCEPTION_LOG_MESSAGE = "Thread ha been interrupted";
+
+    public UserRecallProducer(CallCenterService callCenterService) {
+        this.callCenterService = callCenterService;
+    }
+
+    @Override
+    public void run() {
+
+        while (true) {
+            try {
+                User user = callCenterService.takeUserFromUserCache();
+                user.recall();
+                callCenterService.saveUser(user);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                log.error(INTERRUPTED_EXCEPTION_LOG_MESSAGE, e);
+            }
+        }
+    }
 }
