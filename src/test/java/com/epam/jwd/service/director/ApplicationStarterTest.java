200f61bdada045bff2c9b5dc1fda2e28d18f0ba9
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-09-27 02:18:13 +0300

    Create ApplicationStarterTest

---
+++ src/test/java/com/epam/jwd/service/director/ApplicationStarterTest.java
@@ -0,0 +1,31 @@
+package com.epam.jwd.service.director;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class ApplicationStarterTest {
+
+    private ApplicationStarter applicationStarter;
+
+    @BeforeAll
+    void beforeAll() {
+        this.applicationStarter = new ApplicationStarter();
+    }
+
+    @BeforeEach
+    void setUp() {
+        applicationStarter.start(4);
+    }
+
+    @Test
+    void startWithFourOperators() {
+        assertEquals(8, Thread.activeCount());
+    }
+
+}
