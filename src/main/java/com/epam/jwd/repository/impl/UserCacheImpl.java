ba621147f8fc06d1544032fc7786ad8c2b216787
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-09-26 13:23:18 +0300

    Add userCache implementation

---
+++ src/main/java/com/epam/jwd/repository/impl/UserCacheImpl.java
@@ -0,0 +1,34 @@
+package com.epam.jwd.repository.impl;
+
+import com.epam.jwd.repository.api.UserCache;
+import com.epam.jwd.repository.entity.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserCacheImpl implements UserCache<String, User> {
+
+    private static UserCacheImpl instance = new UserCacheImpl();
+    private final List<User> userCache = new ArrayList<>();
+
+    private UserCacheImpl() {
+    }
+
+    public static UserCacheImpl getInstance() {
+        if(instance != null) {
+            instance = new UserCacheImpl();
+        }
+
+        return instance;
+    }
+
+    @Override
+    public boolean addToUserCache(User user) {
+        return userCache.add(user);
+    }
+
+    @Override
+    public boolean removeFromUserCache(User user) {
+        return userCache.remove(user);
+    }
+}
