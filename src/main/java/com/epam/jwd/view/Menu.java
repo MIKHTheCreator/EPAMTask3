0b42980fbdbafdb89178691253c0d9df29912026
Mikhail Kharevich <81559818+MIKHTheCreator@users.noreply.github.com>
2021-09-26 18:26:38 +0300

    Refactor code

--- src/main/java/com/epam/jwd/view/Menu.java
+++ src/main/java/com/epam/jwd/view/Menu.java
@@ -9,11 +9,10 @@ public class Menu {
             This is a test multi-threading application with little amount of opportunities
             You have only one:
             1.Choose the number of operators of this CallCenter
-            Now we have 4 active operators in our center!!!
-                        
+            Now we have 4 active operators in our center and 30 clients!!!
             """;
-    private static final String LETS_START = "//Enter any key to start:\n";
-    private static final String APPLICATION_STARTING = "Application starting, enjoy the result!\n";
+    private static final String LETS_START = "//Press Enter button:";
+    private static final String APPLICATION_STARTING = "Application starting, enjoy the result!";
 
 
     public void getStarterMenu() {
